dcf15c4aca63c87eb2433f2579b17449b674148f
saed-gpr <saedgholipour81@gmail.com>
2026-07-06 15:24:01 +0330

    feat: setup database configurations and user-todo models using sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from config import Config
+from app.models import db
 
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        
     from app.routes import main_bp
     app.register_blueprint(main_bp)
 
